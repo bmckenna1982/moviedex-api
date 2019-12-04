57df380c37c20f60a9c687c2b27b8c2f01eab96b
bmckenna1982 <bmckenna1982@gmail.com>
2019-12-04 11:19:12 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const MOVIEDEX = require('./moviedex.json')
+
+const app = express()
+
+app.use(morgan('common'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+  const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN
+
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request'})
+  }
+  next()
+})
+
+app.get('/movie', (req, res) => {
+  const { genre, country, avg_vote } = req.query
+  let response = MOVIEDEX
+
+  if(genre) {
+    response = response.filter(movie => 
+    movie.genre.toLowerCase().includes(genre.toLowerCase()))
+  }
+
+  if(country) {
+    response = response.filter(movie => 
+      movie.country.toLowerCase().includes(country.toLowerCase()))
+  }
+
+  if(avg_vote) {    
+    response = response.filter(movie =>
+      movie.avg_vote >= Number(avg_vote))
+  }
+
+  res.json(response)
+})
+
+module.exports = app
